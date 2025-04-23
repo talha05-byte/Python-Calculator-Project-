1fc9250a1ae6d70c185597b217a8fdb55083a807
talha05-byte <talhanayyar820@gmail.com>
2025-04-23 06:50:50 +0400

    Updated Version of Calculator Project .py
    Added Remainder Mode %>>>
    Added My Info Mode #Explains simple info about me :) 
    Removed some bugs and corrections ^-^ .

--- Calculator Project .py
+++ Calculator Project .py
@@ -7,7 +7,7 @@ class Calculator:
         self.temp = 0.0
 
     def add(self):
-        print("~~~~~~~ Welcome to Add ~~~~~~~~~~")
+        print("~~~~~~~ Welcome to Add Mode ~~~~~~~~~~")
         str_firstnum = input("Please enter the first number: ")
         str_secondnum = input("Please enter the second number: ")
         try:
@@ -20,7 +20,7 @@ class Calculator:
             print("Please enter numbers only")
 
     def sub(self):
-        print("~~~~~~~ Welcome to Subtract ~~~~~~~~~~")
+        print("~~~~~~~ Welcome to Subtract Mode ~~~~~~~~~~")
         str_firstnum = input("Please enter the first number: ")
         str_secondnum = input("Please enter the second number: ")
         try:
@@ -41,7 +41,7 @@ class Calculator:
             print("Please enter numbers only")
 
     def mul(self):
-        print("~~~~~~~ Welcome to Multiplication ~~~~~~~~~~")
+        print("~~~~~~~ Welcome to Multiplication Mode ~~~~~~~~~~")
         str_firstnum = input("Please enter the first number: ")
         str_secondnum = input("Please enter the second number: ")
         try:
@@ -54,7 +54,7 @@ class Calculator:
             print("Please enter numbers only")
 
     def div(self):
-        print("~~~~~~~ Welcome to Division ~~~~~~~~~~")
+        print("~~~~~~~ Welcome to Division Mode ~~~~~~~~~~")
         str_firstnum = input("Please enter the first number: ")
         str_secondnum = input("Please enter the second number: ")
         try:
@@ -68,15 +68,41 @@ class Calculator:
         except ValueError:
             print("\nError :( ")
             print("Please enter numbers only")
-
+    def mod(self):
+        print("~~~~~~~ Welcome to Remainder Mode ~~~~~~~~~~")
+        str_firstnum = input("Please enter the first num: ")
+        str_secondnum = input("Please enter the second number: ")
+        try:
+            self.firstnum = float(str_firstnum)
+            self.secondnum = float(str_secondnum)
+            if self.secondnum != 0.0:
+                self.result = (self.firstnum % self.secondnum)
+                print(f"The remainder is = {self.result}")
+            else:
+                print("Error! Number cannot be divided by zero...")
+        except:
+            print("\n Error :( ")
+            print("Please enter numbers only")
+   
+    def info(self):
+        
+        print("\nMy name is Muhammad Talha Nayyar")
+        print("I am a 19 year old computer science enthusiast")
+        print("Currently I am a basic python programmer") 
+        print("Willing to learn new things ^-^ ")
+        print("More projects coming in the future... ")
+    
     def front_end_user(self):
         try:
+            
             while True:
                 print("\n~~~~~~ Welcome to the Calculator App ~~~~~~~")
                 print("Enter '+' for Addition")
                 print("Enter '-' for Subtraction")
                 print("Enter '*' for Product")
                 print("Enter '/' for  Division")
+                print("Enter '%' for Remainder")
+                print("Enter ^-_-^ for my Info")
                 print("Enter CRTL+C TO EXIT")
                 self.options = input("Enter a choice ranging from '+','-','*','/','%': ")
 
@@ -88,9 +114,14 @@ class Calculator:
                     self.mul()
                 elif self.options == "/":
                     self.div()
+                elif self.options == "%":
+                    self.mod()
+                elif self.options == "^-_-^":
+                    self.info()
                 else:
                     print("\nError...")
                     print("Please enter from these options '+','-','*','/','%' ")
+        
         except KeyboardInterrupt:
             print("\nExiting from the Calculator App...")
 
